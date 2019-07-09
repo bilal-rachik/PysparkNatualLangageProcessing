035084910061de74dfe450d29176bca662bc4451
rachikbilal <brachik@bigapps.fr>
2019-07-09 12:34:28 +0200

    =m

--- prediction.py
+++ prediction.py
@@ -39,7 +39,7 @@ def pred():
     
     return json.jsonify(df)
 if __name__=="__main__":
-    app.run(debug=True,host='0.0.0.0',port=5050 )
+    app.run(debug=True,host='0.0.0.0',port=80 )
     app.ru
 
 
